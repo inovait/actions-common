e467b044df18867e4680883370cf2fc0dfa78544
Matej Drobnic <matej.drobnic@inova.si>
2023-01-20 13:38:37 +0100

    feat(jira-get-tickets-from-commit): add onlyResolvedTickets property

--- jira-get-tickets-from-commits/src/index.ts
+++ jira-get-tickets-from-commits/src/index.ts
@@ -12,11 +12,12 @@ async function main(): Promise<void> {
   try {
     const toCommit: string = core.getInput('to', { required: true })
     const fromCommit: string = core.getInput('from', { required: true })
+    const onlyResolvedTickets = core.getBooleanInput('onlyResolvedTickets')
 
     const repo = await Repository.open('.')
     const commits = await gatherCommits(repo, fromCommit, toCommit)
 
-    const tickets = getJiraTickets(commits)
+    const tickets = getJiraTickets(commits, onlyResolvedTickets)
     core.setOutput('tickets', tickets.join(','))
   } catch (error: any) {
     console.log(error)
