04d67a100f8466ba0744a9c11b43b2a585b83513
Matej Drobnic <matej.drobnic@inova.si>
2023-01-20 11:08:42 +0100

    refactor(jira-get-tickets-from-commit): use common commit parser

--- jira-get-tickets-from-commits/src/detector.ts
+++ jira-get-tickets-from-commits/src/detector.ts
@@ -1,31 +1,15 @@
 import { Commit } from 'nodegit'
+import { parseCommits } from 'action_common_libs/src/commit-parsing'
 
 export function getJiraTickets(commits: Commit[]): string[] {
+  const parsedCommits = parseCommits(commits)
   const tickets: string[] = []
 
-  for (const commit of commits) {
-    searchForTickets(commit.message(), tickets)
-    const body = commit.body()
-    if (body != null) {
-      searchForTickets(body, tickets)
+  for (const commit of parsedCommits) {
+    if (commit.jiraTicket != null) {
+      tickets.push(commit.jiraTicket)
     }
   }
 
   return tickets
 }
-
-function searchForTickets(text: any, target: string[]): void {
-  while (true) {
-    const match = JIRA_REGEX.exec(text)
-    if (match == null) {
-      break
-    }
-
-    const ticketName = match[0]
-    if (!target.includes(ticketName)) {
-      target.push(ticketName)
-    }
-  }
-}
-
-const JIRA_REGEX = /[A-Z]{2,4}-[0-9]+/g
