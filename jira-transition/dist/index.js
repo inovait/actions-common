7aab70cc0d2d97227d93c10598f7737b5c1b00c7
Build <ci@inova.si>
2023-06-06 08:59:08 +0000

    ci: build actions

--- jira-transition/dist/index.js
+++ jira-transition/dist/index.js
@@ -48,6 +48,7 @@ function main() {
             const from = (_b = (_a = core.getInput('from')) === null || _a === void 0 ? void 0 : _a.toLowerCase()) === null || _b === void 0 ? void 0 : _b.trim();
             const to = (_c = core.getInput('to', { required: true }).toLowerCase()) === null || _c === void 0 ? void 0 : _c.trim();
             let resolution = core.getInput('resolution');
+            console.log('start res: ', resolution);
             const jira = yield (0, jira_1.getJiraClient)();
             const tickets = yield (0, jira_1.queryJiraTickets)(jira);
             for (const ticket of tickets) {
