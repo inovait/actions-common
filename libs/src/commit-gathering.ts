33f3c682e78654bc823113b54dfc1e606088c17c
Matej Drobnic <matej.drobnic@inova.si>
2023-09-28 13:39:21 +0200

    docs: update git-related actions to v3

--- libs/src/commit-gathering.ts
+++ libs/src/commit-gathering.ts
@@ -1,6 +1,8 @@
 import * as child_process from 'node:child_process'
 
 export function gatherCommits(folder: string, fromSha: string, toSha: string): Commit[] {
+  // Use git log to efficiently list all commits
+  // based on https://gist.github.com/cekstam/a7758b8f315835d479f379715eebd0c3
   const res = child_process.execSync(
       `git log ${fromSha}..${toSha} \\
   --pretty=format:'{^^^^hash^^^^:^^^^%H^^^^,^^^^date^^^^:^^^^%cI^^^^,^^^^parents^^^^:^^^^%P^^^^,^^^^summary^^^^:^^^^%s^^^^,^^^^body^^^^:^^^^%b^^^^}!!ZZ!!' \\
