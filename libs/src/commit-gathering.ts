b4fbb47ad7a74a92fd618b6c2f135f6c176a2f49
Kristjan Kotnik <126757612+inovakotnik@users.noreply.github.com>
2023-10-02 11:58:03 +0200

    fix: escape carriage return

--- libs/src/commit-gathering.ts
+++ libs/src/commit-gathering.ts
@@ -16,7 +16,7 @@ export function gatherCommits(folder: string, fromSha: string, toSha: string): C
   return res.toString().split('!!ZZ!!')
     .filter(commitStr => commitStr.trim().length > 0)
     .map(commitStr => {
-      const singleLineCommitStr = commitStr.trim().replace(/\n/g, '\\n').replace(/\t/g, '\\t').replace(/\x01/g, ' ')
+      const singleLineCommitStr = commitStr.trim().replace(/\n/g, '\\n').replace(/\r/g, '\\r').replace(/\t/g, '\\t').replace(/\x01/g, ' ')
       console.log(singleLineCommitStr); return JSON.parse(singleLineCommitStr) as Commit
     })
 }
